ffa84518d18a9889072a8e6c0cd10c358062fddd
Francois ROLAND <francois.roland@umons.ac.be>
2019-10-27 14:51:56 +0100

    Teacher code.

--- 20191027_thibault2.py
+++ 20191027_thibault2.py
@@ -1,44 +1,48 @@
 from turtle import *
-from math import pi, sin, cos, sqrt, acos, asin, atan2
+from math import pi, sin, cos
 
-def hexagone(point, longueur, col) :
+ANGLE = pi / 3
+
+
+def hexagone(point, longueur, col):
     x = point[0]
     y = point[1]
+
+    up()
+    goto(x, y)
+    down()
+
     color(col[0])
-    a = pi / 3
-    r = 50
     begin_fill()
-    goto(x+r * cos(0 * a), (y+r * sin(0 * a)))
-    goto(x+r * cos(1 * a), (y+r * sin(1 * a)))
-    goto(x+r * cos(2 * a), (y+r * sin(2 * a)))
-    goto(x+r * 0 * cos(0 * a) , (y+r * 0 * sin(0)))
+    goto(x + longueur * cos(0 * ANGLE), (y + longueur * sin(0 * ANGLE)))
+    goto(x + longueur * cos(1 * ANGLE), (y + longueur * sin(1 * ANGLE)))
+    goto(x + longueur * cos(2 * ANGLE), (y + longueur * sin(2 * ANGLE)))
+    goto(x + longueur * 0 * cos(0 * ANGLE), (y + longueur * 0 * sin(0)))
     end_fill()
 
-    begin_fill()
     color(col[1])
-    goto((x+r * cos(0 * a)),(y+r * -sin(0 * a)))
-    goto(x+r * cos(1 * a), (y+r * -sin(1 * a)))
-    goto(x+r * cos(2 * a), y+r * -sin(2 * a))
-    goto(x+r * 0 * cos(0 * a) , (y+r * 0 * sin(0)))
-    end_fill()
-
     begin_fill()
-    color(col[2])
-    goto(x+r * cos(2 * a), (y+r * sin(2 * a)))
-    goto(x+r * cos(3 * a), (y+r * sin(3 * a)))
-    goto(x+r * cos(4 * a), (y+r * sin(4 * a)))
-    goto(x+r * 0 * cos(0 * a) , (y+r * 0 * sin(0)))
+    goto((x + longueur * cos(0 * ANGLE)), (y + longueur * -sin(0 * ANGLE)))
+    goto(x + longueur * cos(1 * ANGLE), (y + longueur * -sin(1 * ANGLE)))
+    goto(x + longueur * cos(2 * ANGLE), y + longueur * -sin(2 * ANGLE))
+    goto(x + longueur * 0 * cos(0 * ANGLE), (y + longueur * 0 * sin(0)))
     end_fill()
 
-    done()
+    color(col[2])
+    begin_fill()
+    goto(x + longueur * cos(2 * ANGLE), (y + longueur * sin(2 * ANGLE)))
+    goto(x + longueur * cos(3 * ANGLE), (y + longueur * sin(3 * ANGLE)))
+    goto(x + longueur * cos(4 * ANGLE), (y + longueur * sin(4 * ANGLE)))
+    goto(x + longueur * 0 * cos(0 * ANGLE), (y + longueur * 0 * sin(0)))
+    end_fill()
 
-color1="green"
-color2="yellow"
-color3="blue"
-col = color1,color2,color3
+
+col = ("green", "yellow", "blue")
 inf_gauche = -315
 sup_droit = 315
-centre = (-50, - 50 , -50)
+centre = (-50, - 50, -50)
 point = -315, -315
 longueur = 30
 hexagone(point, longueur, col)
+
+done()
