297f844ce4f47544300fb0e9522ce9b391a0e8ef
Francois ROLAND <francois.roland@umons.ac.be>
2019-10-27 15:23:21 +0100

    Fix code errors.

--- 20191027_thibault1.py
+++ 20191027_thibault1.py
@@ -5,54 +5,52 @@ ANGLE = pi / 3
 
 
 def hexagone(point, longueur, col, centre, rayon):
-    rayon = 50
+    x, y = point
 
     up()
-    goto(point)
+    goto(x, y)
     down()
 
     color(col[0])
     begin_fill()
-    goto(int(rayon * cos(0 * ANGLE)), int(rayon * sin(0 * ANGLE)))
-    goto(int(rayon * cos(1 * ANGLE)), int(rayon * sin(1 * ANGLE)))
-    goto(int(rayon * cos(2 * ANGLE)), int(rayon * sin(2 * ANGLE)))
-    goto(point)
+    goto(x + int(longueur * cos(0 * ANGLE)), y + int(longueur * sin(0 * ANGLE)))
+    goto(x + int(longueur * cos(1 * ANGLE)), y + int(longueur * sin(1 * ANGLE)))
+    goto(x + int(longueur * cos(2 * ANGLE)), y + int(longueur * sin(2 * ANGLE)))
+    goto(x, y)
     end_fill()
 
     color(col[1])
     begin_fill()
-    goto(point)
-    goto(int(rayon * cos(0 * ANGLE)), int(rayon * -sin(0 * ANGLE)))
-    goto(int(rayon * cos(1 * ANGLE)), int(rayon * -sin(1 * ANGLE)))
-    goto(int(rayon * cos(2 * ANGLE)), int(rayon * -sin(2 * ANGLE)))
-    goto(point)
+    goto(x, y)
+    goto(x + int(longueur * cos(0 * ANGLE)), y + int(longueur * -sin(0 * ANGLE)))
+    goto(x + int(longueur * cos(1 * ANGLE)), y + int(longueur * -sin(1 * ANGLE)))
+    goto(x + int(longueur * cos(2 * ANGLE)), y + int(longueur * -sin(2 * ANGLE)))
+    goto(x, y)
     end_fill()
 
     color(col[2])
     begin_fill()
-    goto(int(rayon * cos(2 * ANGLE)), int(rayon * sin(2 * ANGLE)))
-    goto(int(rayon * cos(3 * ANGLE)), int(rayon * sin(3 * ANGLE)))
-    goto(int(rayon * cos(4 * ANGLE)), int(rayon * sin(4 * ANGLE)))
+    goto(x + int(longueur * cos(2 * ANGLE)), y + int(longueur * sin(2 * ANGLE)))
+    goto(x + int(longueur * cos(3 * ANGLE)), y + int(longueur * sin(3 * ANGLE)))
+    goto(x + int(longueur * cos(4 * ANGLE)), y + int(longueur * sin(4 * ANGLE)))
     goto(point)
     end_fill()
 
+
+def pavage(inf_gauche, sup_droit, longueur, col, centre, rayon):
+    deplacement_horizontal = 3 * longueur
+    deplacement_vertical = int(longueur * sin(ANGLE))
+    ligne = 1
+
+    for y in range(inf_gauche, sup_droit, deplacement_vertical):
+        if ligne % 2 == 0:
+            decalage_ligne = int(1.5 * longueur)
+        else:
+            decalage_ligne = 0
+        for x in range(inf_gauche + decalage_ligne, sup_droit, deplacement_horizontal):
+            hexagone((x, y), longueur, col, centre, rayon)
+        ligne = ligne + 1
     done()
 
 
-hexagone((0, 0), 50, ('blue', 'yellow', 'green'), (0, 0, 0), 0)
-
-
-def pavage(inf_gauche, sup_droit, longueur, col, centre, rayon):
-    c = 1
-    a = pi / 3
-    sup_d = sup_droit
-    es_p = 3 * longueur
-    inf_g = inf_gauche
-    p_depart = int(inf_gauche - longueur * (1 + cos(a)))
-    p_final = int(sup_droit + longueur * (1 + cos(a)))
-
-    while p_final > p_depart:
-        print(p_depart, p_final)
-        if c % 2 != 0:
-            for i in range(inf_gauche, sup_droit, es_p):
-                hexagone((0, 0), 50, ('blue', 'yellow', 'green'), (0, 0, 0), 0)
+pavage(-150, 150, 30, ('blue', 'yellow', 'green'), (0, 0, 0), 0)
